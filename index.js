e5a633b86a8f9d327bcf8222cd78f4f06572c716
John J Czaplewski <john@czaplewski.org>
2017-08-04 08:49:26 -0500

    Clean up, comment, validate params

--- index.js
+++ index.js
@@ -3,60 +3,83 @@
 const fs = require('fs')
 const exec = require('child_process').exec
 const request = require('request')
-const cover = require('@mapbox/tile-cover')
-const buffer = require('@turf/buffer')
-const envelope = require('@turf/envelope')
-const bbox = require('@turf/bbox')
 const bboxPolygon = require('@turf/bbox-polygon')
+const cover = require('@mapbox/tile-cover')
 const async = require('async')
 const sharp = require('sharp')
 
-const TEMP_DIR = `${__dirname}/tmp`
-
 sharp.cache(0)
 
-module.exports = (coord, hollaback) => {
-  let point = {
-    "type": "Point",
-    "coordinates": [-89, 43]
+// Define constants
+const TEMP_DIR = `${__dirname}/tmp`
+const VALID_FORMATS = ['jpeg', 'tiff', 'png']
+const MIN_ZOOM = 0
+const MAX_ZOOM = 15
+
+const defaults = {
+  format: 'jpeg',
+  zoom: 0,
+  extent: [-180, -85, 180, 85]
+}
+
+/*
+@input
+  extent: [minlng minlat maxlng maxlat]
+  options:
+    format: jpeg, tiff, png
+    zoom: integer
+
+@output
+  Buffer in the specified format
+*/
+module.exports = (extent, options, hollaback) => {
+  // Validate parameters
+  if (extent.length < 4 || extent[0] < -180 || extent[1] < -85 || extent[2] > 180 || extent[3] > 85) {
+    return hollaback('Invalid extent')
+  }
+  // Validate each coordinate in the extent
+  extent.forEach(coord => {
+    if (isNaN(coord)) {
+      return hollaback('Extent contains an invalid coordinate')
+    }
+  })
+
+  // Validate provided zoom level
+  if (options && options.zoom && (options.zoom < MIN_ZOOM || options.zoom > MAX_ZOOM)) {
+    return hollaback(`Invalid zoom. Zoom must be between ${MIN_ZOOM} and %{MAX_ZOOM}`)
   }
 
-  let bigBuffer = envelope(buffer(point, 4, 'miles'))
-  let smallBuffer = envelope(buffer(point, 2, 'miles'))
+  // Validate provided format
+  if (options && options.format && VALID_FORMATS.indexOf(options.format) === -1) {
+    return hollaback(`Invalid format. Choose one of the following: ${VALID_FORMATS.join(', ')}`)
+  }
 
-  let minLats = smallBuffer.geometry.coordinates[0].map(coords => {
-    return coords[1]
-  })
+  // Use provided parameters or defaults
+  extent = extent || defaults.extent
+  let zoom = options.zoom || defaults.zoom
+  let format = options.format || defaults.format
+  const fileName = Math.random().toString(36).substring(15)
 
-  let minLat = Math.min.apply(null, minLats)
-  let maxLat = Math.max.apply(null, minLats)
+  // Create a geojson polygon to feed to tile-cover
+  let bounds = bboxPolygon(extent)
 
-  // let minLat = Math.min(...minLats)
-  // let maxLat = Math.max(...minLats)
-
-  let maxLngs = bigBuffer.geometry.coordinates[0].map(coords => {
-    return coords[0]
-  })
-
-  let minMaxLng = Math.min.apply(null, maxLngs)
-  let maxMaxLng = Math.max.apply(null, maxLngs)
-
-  // let minMaxLng = Math.min(...maxLngs)
-  // let maxMaxLng = Math.max(...maxLngs)
-
-  let pBbox = [ minMaxLng, minLat, maxMaxLng, maxLat ]
-
-  let bounds = bboxPolygon(pBbox)
-
+  // Generate a list of tiles needed to cover the provided extent
   let tiles = cover.tiles(bounds.geometry, {
-    min_zoom: 12,
-    max_zoom: 12
+    min_zoom: zoom,
+    max_zoom: zoom
   })
+
+  if (tiles.length > 50) {
+    return hollaback(`Too many tiles are needed to cover this area. Please
+      choose a smaller extent or a smaller zoom level.`)
+  }
+  if (tiles.length === 0) {
+    return hollaback(`The provided extent is a negative area.`)
+  }
+
   let tilePaths = tiles.map(tile => {
     return `${TEMP_DIR}/${tile[2]}_${tile[0]}_${tile[1]}.tif`
   })
-
-  const fileName = Math.random().toString(36).substring(15)
 
   async.waterfall([
     (callback) => {
@@ -75,6 +98,7 @@ module.exports = (coord, hollaback) => {
       })
     },
 
+    // Combine the elevation tiles to a single raster
     (callback) => {
       exec(`gdalwarp -wm 4000 ${tilePaths.join(' ')} ${TEMP_DIR}/${fileName}_merged.tif`, (error, stdout, stderror) => {
         if (error) {
@@ -84,13 +108,15 @@ module.exports = (coord, hollaback) => {
       })
     },
 
+    // Crop the merged tiles to the desired extent
     (callback) => {
-      exec(`gdalwarp -te_srs EPSG:4326 -te ${pBbox.join(' ')} ${TEMP_DIR}/${fileName}_merged.tif ${TEMP_DIR}/${fileName}_clipped.tif`, (error, stdout, stderr) => {
+      exec(`gdalwarp -te_srs EPSG:4326 -te ${extent.join(' ')} ${TEMP_DIR}/${fileName}_merged.tif ${TEMP_DIR}/${fileName}_clipped.tif`, (error, stdout, stderr) => {
         if (error) return callback(error)
         callback(null)
       })
     },
 
+    // Generate the hillshade
     (callback) => {
       exec(`gdaldem hillshade -s 3 -az 315 -z 4  ${TEMP_DIR}/${fileName}_clipped.tif ${TEMP_DIR}/${fileName}_shaded.tif`, (error, stdout, stderr) => {
         if (error) return callback(error)
@@ -98,13 +124,20 @@ module.exports = (coord, hollaback) => {
       })
     },
 
+    // Convert the resultant shaded relief tif to the desired format
     (callback) => {
-      let buffer = sharp(`${TEMP_DIR}/${fileName}_shaded.tif`).toBuffer('jpeg', (error, data, info) => {
-        if (error) return callback(error)
-        callback(null, data)
-      })
+      sharp(`${TEMP_DIR}/${fileName}_shaded.tif`)
+        .toFormat(format)
+        .toBuffer()
+        .then(outputBuffer => {
+          callback(null, outputBuffer)
+        })
+        .catch(error => {
+          callback(error)
+        })
     },
 
+    // Clean up temporary files
     (jpeg, callback) => {
       let toDelete = tilePaths.concat([ `${TEMP_DIR}/${fileName}_merged.tif`, `${TEMP_DIR}/${fileName}_clipped.tif`, `${TEMP_DIR}/${fileName}_shaded.tif`])
 
